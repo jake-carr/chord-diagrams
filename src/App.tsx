aaa64c0717461752a4de6716387b22147bf7b0bf
Jake Carr <jcarr0157@gmail.com>
2021-07-12 12:01:11 -0700

    chord diagram project setup and tested fetch api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC, useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+    const [note, selectNote] = useState('D');
+    const [major, toggleQuality] = useState(true);
+    const [diagram, setDiagram] = useState('');
+
+    // const parser = (uberChord: string) => {
+    //     // comes as: "1 3 3 2 1 1" [6th to 1st str]
+    // };
+
+    useEffect(() => {
+        console.log(note, major);
+        fetch(`https://api.uberchord.com/v1/chords/${note}${major ? '' : '_m'}`)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data[0]) {
+                    console.log(data[0]);
+                    // setDiagram(parser(data[0].strings));
+                }
+            });
+    }, [note, major]);
+
+    useEffect(() => {
+        console.log(diagram);
+    }, [diagram]);
+
+    return (
+        <div className="App">
+            <button onClick={() => selectNote('D')}>D</button>
+            <button onClick={() => selectNote('F')}>F</button>
+            <button onClick={() => toggleQuality(!major)}>{major ? 'major' : 'minor'}</button>
+        </div>
+    );
+};
 
 export default App;
