4626684b2a46af4e480691cca4d474edfcc9471f
Jake Carr <jcarr0157@gmail.com>
2021-07-12 12:44:06 -0700

    adds ChordJS lib for generating html chord diagrams and parse  uberChord api data to work with its syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,56 @@
 import React, { FC, useEffect, useState } from 'react';
+import ChordJS from './chords.js';
 import './App.css';
 
 const App: FC = () => {
     const [note, selectNote] = useState('D');
     const [major, toggleQuality] = useState(true);
-    const [diagram, setDiagram] = useState('');
 
-    // const parser = (uberChord: string) => {
-    //     // comes as: "1 3 3 2 1 1" [6th to 1st str]
-    // };
+    const parser = {
+        str: function (uberChord: string) {
+            let result = uberChord.split(' ');
+            for (let char of result) {
+                if (char === 'X') char = char.toLowerCase();
+            }
+            return result.join('');
+        },
+        fingers: function (uberChord: string) {
+            let result = uberChord.split(' ');
+            for (let char of result) {
+                if (char === 'X') char = '-';
+            }
+            return result.join('');
+        },
+    };
 
     useEffect(() => {
-        console.log(note, major);
-        fetch(`https://api.uberchord.com/v1/chords/${note}${major ? '' : '_m'}`)
+        let url = `https://api.uberchord.com/v1/chords/${note}`;
+        if (!major) url += '_m';
+        fetch(url)
             .then((response) => response.json())
             .then((data) => {
                 if (data[0]) {
-                    console.log(data[0]);
-                    // setDiagram(parser(data[0].strings));
+                    const { strings, fingering } = data[0];
+                    const { str, fingers } = parser;
+                    const chart = ChordJS.generate(note, str(strings), fingers(fingering), 3);
+                    const container = document.getElementById('target');
+                    if (container) {
+                        while (container.firstChild) {
+                            container.removeChild(container.firstChild);
+                        }
+                        container.appendChild(chart);
+                    }
                 }
             });
     }, [note, major]);
 
-    useEffect(() => {
-        console.log(diagram);
-    }, [diagram]);
-
     return (
         <div className="App">
+            <button onClick={() => selectNote('A')}>A</button>
             <button onClick={() => selectNote('D')}>D</button>
             <button onClick={() => selectNote('F')}>F</button>
             <button onClick={() => toggleQuality(!major)}>{major ? 'major' : 'minor'}</button>
+            <div id="target"></div>
         </div>
     );
 };
